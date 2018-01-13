6e5e8e49f2f1e4e95a0039152b6da75af6ca0c7c
Avani Ranade <32409549+ranadeavani@users.noreply.github.com>
2018-01-13 15:56:53 +0530

    Add files via upload

---
+++ bon.sql
@@ -0,0 +1,7 @@
+SELECT * FROM bon.user;
+use bon;
+alter table user add constraint primary key(username);
+insert into user values("sheetal","rathore","srathore","jaipur","123");
+select * from user;
+insert into item values(1,0,'Pizza',6,'');
+select * from user;
